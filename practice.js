6e6618cc62c38391c03b3c756f5d7552a81ca925
Akhilgadde18 <akhilgadde1822@gmail.com>
2025-09-06 12:34:04 +0530

    working on muktiple operations

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Helllo World,Akhil")
+
+let name = "Akhil";
+console.log(name )
+
+let num = 18;
+console.log(num)
+
+let isLogged= false;
+console.lod(isLogged)
+
+let x;
+console.log(x)
+
+let y=null;
+
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:akhil
+    age:18
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
